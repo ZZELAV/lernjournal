42d1dc79ec341711fea767339f1ed7186f7de9e9
ZZELAV <valentino.panico@icloud.com>
2022-03-25 15:50:27 +0100

    added php code snippets

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -155,7 +155,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['powershell', 'java'],
+        additionalLanguages: ['powershell', 'java', 'php'],
       },
     }),
 };
