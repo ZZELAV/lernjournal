3fd7f28721955547d437034b1947ce0bfad76435
ZZELAV <valentino.panico@icloud.com>
2022-04-30 09:53:09 +0200

    new logo

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
         title: 'Lernjournal',
         logo: {
           alt: 'Docusaurus Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
